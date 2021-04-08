659f8c0158da34b91f3f1b24df5009b277600cb5
Ben Selfridge <benselfridge@galois.com>
2021-04-08 14:35:18 -0700

    Cleans up Utils.hs

--- src/Maze/Utils.hs
+++ src/Maze/Utils.hs
@@ -5,27 +5,6 @@ module Maze.Utils
 import Math.Combinat.Permutations
 
 import System.Random
--- import System.Random.Shuffle
-
--- -- | Like 'shuffle', but returns the generator.
--- shuffleG :: RandomGen gen => [a] -> Int -> gen -> ([a], gen)
--- shuffleG elements len gen =
---   let (is, gens) = rseq len gen
---       es = shuffle elements is
---       gen' = if null gens then gen else last gens
---   in (es, gen')
---     where
---       -- The sequence (r1,...r[n-1]) of numbers such that r[i] is an
---       -- independent sample from a uniform random distribution
---       -- [0..n-i]
---       rseq :: RandomGen gen => Int -> gen -> ([Int], [gen])
---       rseq n = unzip . rseq' (n - 1)
---           where
---             rseq' :: RandomGen gen => Int -> gen -> [(Int, gen)]
---             rseq' 0 gen = []
---             rseq' i gen = (j, gen) : rseq' (i - 1) gen'
---                 where
---                   (j, gen') = randomR (0, i) gen
 
 shuffle :: RandomGen g => [a] -> g -> ([a], g)
 shuffle as g =
